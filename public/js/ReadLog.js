ded1e30b542758765da36db5beb7453709d99dfe
amdhj22 <amdhj22@gmail.com>
2020-08-11 15:11:20 +0900

    size_modified

--- public/js/ReadLog.js
+++ public/js/ReadLog.js
@@ -37,7 +37,7 @@ socket.on('init_list', function (data) {
 
                 var label = document.createElement('label');
                 label.setAttribute('for', data[i] + '_cb');
-                label.innerHTML = data[i].slice(0, data[i].indexOf('_info'));
+                label.innerHTML = data[i].slice(data[i].indexOf(':')+1, data[i].indexOf('_info'));
 
                 gpulist.appendChild(temp);
                 gpulist.appendChild(label);
@@ -91,16 +91,20 @@ socket.on('data', function (filename, data ) {
                 var tr2 = table.lastChild;
     
                 var td1 = tr1.firstChild;
-                td1.innerHTML = "Fan. &nbsp " + data[i].Fan;
+                var td1_2 = td1.firstChild.lastChild;
+                td1_2.innerHTML = data[i].Temp + " &degC";
     
                 var td2 = tr1.lastChild;
-                td2.innerHTML = "Mem. &nbsp " + data[i].Mem_Per + " %";
+                var td2_2 = td2.firstChild.lastChild;
+                td2_2.innerHTML = data[i].Mem_Per + " %";
     
                 var td3 = tr2.firstChild;
-                td3.innerHTML = "Temp. &nbsp " + data[i].Temp + " &degC";
+                var td3_2 = td3.firstChild.lastChild;
+                td3_2.innerHTML = data[i].Mem_Used;
     
                 var td4 = tr2.lastChild;
-                td4.innerHTML = "Pwr. &nbsp " + data[i].Pwr;
+                var td4_2 = td4.firstChild.lastChild;
+                td4_2.innerHTML = data[i].Mem_Total + " MB";
             }
         }
         else {
@@ -112,7 +116,7 @@ socket.on('data', function (filename, data ) {
 
             var title = document.createElement('div');
             title.className = 'node';
-            title.innerHTML = filename.slice(0, filename.indexOf('_info'));
+            title.innerHTML = filename.slice(filename.indexOf(':')+1, filename.indexOf('_info'));
     
             root.appendChild(title);
     
@@ -135,6 +139,8 @@ socket.on('data', function (filename, data ) {
                     bottom.className = 'bottom cool';
                 }
                 else {
+                    top.removeAttribute('class');
+                    bottom.removeAttribute('class');
                     top.className = 'top';
                     bottom.className = 'bottom';
                 }
@@ -145,16 +151,55 @@ socket.on('data', function (filename, data ) {
                 var tr2 = document.createElement('tr');
     
                 var td1 = document.createElement('td');
-                td1.innerHTML = "Fan. &nbsp " + data[i].Fan;
+                var tb1 = document.createElement('table');
+                var td1_1 = document.createElement('td');
+                var td1_2 = document.createElement('td');
+                td1_1.innerHTML = "Temp." ;
+                td1_2.innerHTML = data[i].Temp + " &degC";
+                td1_2.className = "align_right";
+
+                tb1.appendChild(td1_1);
+                tb1.appendChild(td1_2);
+                td1.appendChild(tb1);
                 tr1.appendChild(td1);
+
                 var td2 = document.createElement('td');
-                td2.innerHTML = "Mem. &nbsp " + data[i].Mem_Per + " %";
+                var tb2 = document.createElement('table');
+                var td2_1 = document.createElement('td');
+                var td2_2 = document.createElement('td');
+                td2_1.innerHTML = "Usage.";
+                td2_2.innerHTML = data[i].Mem_Per + " %";
+                td2_2.className = "align_right";
+
+                tb2.appendChild(td2_1);
+                tb2.appendChild(td2_2);
+                td2.appendChild(tb2);
                 tr1.appendChild(td2);
+
                 var td3 = document.createElement('td');
-                td3.innerHTML = "Temp. &nbsp " + data[i].Temp + " &degC";
+                var tb3 = document.createElement('table');
+                var td3_1 = document.createElement('td');
+                var td3_2 = document.createElement('td');
+                td3_1.innerHTML = "Memory.";
+                td3_2.innerHTML = data[i].Mem_Used;
+                td3_2.className = "align_right";
+
+                tb3.appendChild(td3_1);
+                tb3.appendChild(td3_2);
+                td3.appendChild(tb3);
                 tr2.appendChild(td3);
+
                 var td4 = document.createElement('td');
-                td4.innerHTML = "Pwr. &nbsp " + data[i].Pwr;
+                var tb4 = document.createElement('table');
+                var td4_1 = document.createElement('td');
+                var td4_2 = document.createElement('td');
+                td4_1.innerHTML = "/";
+                td4_2.innerHTML = data[i].Mem_Total + " MB";
+                td4_2.className = "align_right";
+
+                tb4.appendChild(td4_1);
+                tb4.appendChild(td4_2);
+                td4.appendChild(tb4);
                 tr2.appendChild(td4);
     
                 table.appendChild(tr1);
